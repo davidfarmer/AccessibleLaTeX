218d8854b55c5a012da4fe391b55e21b118b4d9e
David W. Farmer <farmer@aimath.org>
2026-02-23 07:59:32 -0500

    set up basic testing

---
+++ src/test.js
@@ -0,0 +1,14 @@
+
+import { scanForAnomalies } from './main.js'
+import { showErrors } from './scan.js'
+
+export { scanForAnomalies }
+
+if (sourceTextArea.addEventListener) {
+  sourceTextArea.addEventListener('input', function() {
+       allErrors = [];
+//      echosourceTextArea.value = scanForAnomalies(sourceTextArea.value);
+      scannedTextArea.innerHTML = scanForAnomalies(sourceTextArea.value);
+      errorsDisplayArea.innerHTML = showErrors(allErrors);
+  });
+}
